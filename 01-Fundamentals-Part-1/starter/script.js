752b9f72e732c7d95a6b7d908035a3aac4e64862
StephanyEloise <stephany.eloise@hotmail.com>
2024-03-26 07:21:40 -0300

    Switch, small operations

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -219,3 +219,62 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 } else {
     console.log('No one wins the trophy.');
 };
+
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday' 
+        console.log('Plan couse structure.');
+        console.log('Go to coding meetup.');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos.');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples.');
+        break;
+    case 'friday':
+        console.log('Record videos.');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+};
+
+// same thing happens
+
+if (day === 'monday') {
+    console.log('Plan couse structure.');
+    console.log('Go to coding meetup.');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos.');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples.');
+} else if (day === 'friday') {
+    console.log('Record videos.');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+};
+
+
+const age = 23;
+
+// age >= 18 ? console.log('I like to drink wine') :
+//     console.log('I like to drink water');
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+};
+
+console.log(drink2);
