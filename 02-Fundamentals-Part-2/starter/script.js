cdf877d0896c912614d47c3da09955fbc940c3c0
StephanyEloise <stephany.eloise@hotmail.com>
2024-03-28 09:37:49 -0300

    Still learning functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -31,7 +31,7 @@
 
 
 // Function declaration 
-function calcAge1(birthYeah) {
+/* function calcAge1(birthYeah) {
     return 2037 - birthYeah;
 };
 
@@ -71,8 +71,26 @@ function fruitProcessor(apples, oranges) {
 };
 
 console.log(fruitProcessor(2, 3));
+*/
 
+const calcAge = function (birthYeah) { // not the same 
+    return 2037 - birthYeah;
+};
 
+const yearsUntilRetirement = function (birthYeah, firstName) {
+    const age = calcAge(birthYeah);
+    const retirement = 65 - age;
 
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired!`);
+        return -1;
+    };
 
+    // return `${firstName} retires in ${retirement} years.`
+};
 
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
