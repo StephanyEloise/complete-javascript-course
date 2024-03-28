7cef1abb413b6c27e76e87e302075c0de819849b
StephanyEloise <stephany.eloise@hotmail.com>
2024-03-28 09:23:47 -0300

    Dont repeat yourself

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -57,3 +57,22 @@ const yearsUntilRetirement = (birthYeah, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
+
+function cutFruitPieces(fruit) {
+    return fruit * 3;
+};
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} pieces of orange.`;
+    return juice;
+};
+
+console.log(fruitProcessor(2, 3));
+
+
+
+
+
