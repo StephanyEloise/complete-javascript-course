4a446f711e78a639f08e6eea0aa2e725de814c88
StephanyEloise <stephany.eloise@hotmail.com>
2024-04-03 08:26:50 -0300

    Challenge #2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -97,48 +97,48 @@ console.log(fruitProcessor(2, 3));
 
 
 /* CHALLENGE #1
-Back to the two gymnastics teams, 
-the Dolphins and the Koalas! 
+Back to the two gymnastics teams,
+the Dolphins and the Koalas!
 There is a new gymnastics discipline,
  which works differently.
 
 Each team competes 3 times,
-and then the average of the 3 scores 
+and then the average of the 3 scores
 is calculated (so one average score per team).
 
-A team only wins if it has at least double 
-the average score of the other team. 
+A team only wins if it has at least double
+the average score of the other team.
 Otherwise, no team wins!
 
 Your tasks:
 
-Create an arrow function calcAverage 
-to calculate the average of 3 scores. 
-This function should have three parameters 
+Create an arrow function calcAverage
+to calculate the average of 3 scores.
+This function should have three parameters
 and return a single number (the average score).
 
-Create two new variables — scoreDolphins and scoreKoalas, 
-and assign the value returned from the calcAverage function to them 
+Create two new variables — scoreDolphins and scoreKoalas,
+and assign the value returned from the calcAverage function to them
 (you will need to call this function, and pass scores as arguments).
 
-Create a function checkWinner that takes the average 
-score of each team as parameters (avgDolphins and avgKoalas), 
-and then logs the winner to the console, 
-together with the victory points, 
-according to the rule above. Example: Koalas win (30 vs. 13) 
+Create a function checkWinner that takes the average
+score of each team as parameters (avgDolphins and avgKoalas),
+and then logs the winner to the console,
+together with the victory points,
+according to the rule above. Example: Koalas win (30 vs. 13)
 (use avgDolphins and avgKoalas instead of hard-coded values).
 
-Use the checkWinner function to determine 
+Use the checkWinner function to determine
 the winner for both DATA 1 and DATA 2.
 
-Ignore draws this time. Instead, 
-log No team wins... 
+Ignore draws this time. Instead,
+log No team wins...
 to the console if there is no winner.
 
-TEST DATA 1: Dolphins scored 44, 23, and 71. 
+TEST DATA 1: Dolphins scored 44, 23, and 71.
 Koalas scored 65, 54, and 49.
 
-TEST DATA 2: Dolphins scored 85, 54, and 41. 
+TEST DATA 2: Dolphins scored 85, 54, and 41.
 Koalas scored 23, 34, and 27. */
 
 /* const calcAverage = (a, b, c) => (a + b + c) / 3;
@@ -164,36 +164,106 @@ scoreKoalas = calcAverage(23, 34, 27);
 
 checkWinner(scoreDolphins, scoreKoalas); */
 
-const friends = ['Micheal', 'Steven', 'Peter'];
+// const friends = ['Micheal', 'Steven', 'Peter'];
 // const years = new Array(1991, 1984, 2008, 2020);
 
-console.log(friends[0]); // Michael, [1] Steven, [2] Peter
-console.log(friends.length);
-console.log(friends[friends.length - 1]); // Peter
+// console.log(friends[0]); // Michael, [1] Steven, [2] Peter
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]); // Peter
 
-friends[2] = 'Jay'; // Peter -> Jay
-console.log(friends);
+// friends[2] = 'Jay'; // Peter -> Jay
+// console.log(friends);
 
 // Const = [still can change de element inside];
 
-const firstName = 'Jonas'
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// const firstName = 'Jonas'
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
 
-console.log(jonas);
-console.log(jonas.length);
+// console.log(jonas);
+// console.log(jonas.length);
 
-// Exercise 
+// Exercise
 
-const calcAge = function (birthYeah) {
-    return 2037 - birthYeah;
+// const calcAge = function (birthYeah) {
+//     return 2037 - birthYeah;
+// };
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+// console.log(ages);
+
+const friends = ['Micheal', 'Steven', 'Peter'];
+
+// Add Elements
+friends.push('Jay'); // Last
+friends.unshift('Jhon'); // First
+console.log(friends);
+
+// Remove Elements
+friends.pop(); // Last
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven')); // 1 
+console.log(friends.indexOf('Any Name')); // -1
+
+console.log(friends.includes('Steven')); // true 
+console.log(friends.includes('Any Name')); // false
+
+friends.push(23);
+console.log(friends.includes('23')); // false
+console.log(friends.includes(23)); // true
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
 };
 
-const years = [1990, 1967, 2002, 2010, 2018];
+/* CHALLENGE #2
+Steven wants you to improve his tip calculator, 
+using the same rules as before — 
+tip 15% of the bill if the bill value is 
+between 50 and 300, and if the value is different, 
+the tip is 20%.
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
+Your tasks:
 
-const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
-console.log(ages);
+Write a function calcTip that takes any bill value 
+as an input and returns the corresponding tip, 
+calculated based on the rules above 
+(you can check out the code from the 
+first tip calculator challenge if you need to). 
+Use the function type you like the most. 
+Test the function using a bill value of 100.
+
+And now let's use arrays! So, 
+create an array called bills containing 
+the test data below.
+
+Create an array called tips containing 
+the tip value for each bill, calculated 
+from the function you created before.
+
+BONUS: Create an array totals 
+containing the total values, 
+so the bill + tip.
+
+TEST DATA: 125, 555, and 44.
+
+*/
+
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
+
