a9589b9f915212a37333ad6912d498c9b7cf281b
StephanyEloise <stephany.eloise@hotmail.com>
2024-04-11 05:04:15 -0300

    types

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -413,7 +413,29 @@ John weighs 92 kg and is 1.95 m tall. */
 // for loop keeps running while
 // condition is TRUE
 
-for (let rep = 1; rep <= 50; rep++) {
-    console.log(`Lifting weights repetition ${rep}`);
+// for (let rep = 1; rep <= 50; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// };
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+
+    // Reading from jonas array
+    console.log(jonas[i], typeof jonas[i]);
+
+    // Filling types array
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
 };
 
+console.log(types);
