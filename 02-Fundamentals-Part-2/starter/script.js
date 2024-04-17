6c44f720fc6f22f7608d9530639c798b388194bc
StephanyEloise <stephany.eloise@hotmail.com>
2024-04-17 00:35:32 -0300

    Bonus

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -557,4 +557,15 @@ before by the length of the array
 
 Call the function with the totals array. */
 
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        // sum = sum + arr[i];
+        sum += arr[i];
+    }
+    return sum / arr.length;
+};
 
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
