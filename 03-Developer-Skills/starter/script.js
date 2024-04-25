c8c4422f48145f4badb970a69611055791bc8f55
StephanyEloise <stephany.eloise@hotmail.com>
2024-04-24 23:40:40 -0300

    Skills & Editor Setup

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -12,5 +12,29 @@
  const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17,15,9, 5];
 
  // 1) Understanding the pronlem
+ // - What is temp amplitude? 
+ // Difference between highest and lowest temp
+ // - How to compute max and min temperatures?
+ // - What's a sensor error? And what to do?
 
  // 2) Breaking up into sub-pronlems
+// - How to ignore errors?
+// - Find max value in temp array 
+// - Find min value in temp array 
+// - Substract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+    let max = temps[0];
+    let min = temps[0];
+
+    for(let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i];
+
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    };
+    console.log(max, min);
+};
+
+calcTempAmplitude([3, 7, 4, 1, 8]);
+if(temps[i] > max) max = curTemp[i];
