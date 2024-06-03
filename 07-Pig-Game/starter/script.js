c9f0e098c7839db826a38dc6c99c812f02682de7
StephanyEloise <stephany.eloise@hotmail.com>
2024-06-03 08:38:13 -0300

    Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,6 +4,8 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 
 const diceEl = document.querySelector('.dice');
@@ -15,6 +17,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality 
 btnRoll.addEventListener('click', function(){
@@ -29,10 +35,12 @@ btnRoll.addEventListener('click', function(){
     // if true, switch to next player 
     if(dice !==1 ) {
         // Add dice to current score
-        
-        
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        current0El.textContent = currentScore; // Change later
     } else {
         // Switch to next player
+        activePlayer = activePlayer === 0 ? 1 : 0;
     }
 
 });
